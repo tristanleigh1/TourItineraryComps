dd00309ecdbed4994db98f585ae43111698ca6b5
Caleb Braun <calebjbraun@gmail.com>
2016-11-01 15:52:13 -0500

    Preparing to pull

--- mytinerary/tour/views.py
+++ mytinerary/tour/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render
-
 from .models import *
+
 # Create your views here.
 #GET: tour/
 def index(request):
@@ -10,8 +10,23 @@ def index(request):
 def map(request):
 #    poi = get_object_or_404(POI, pk=1)
 #    poi_list = get_list_or_404(POI, city="Minneapolis")
-    poi_list = POI.objects.filter(city="Minneapolis")[:8]
+#    poi_list = POI.objects.filter(city="Minneapolis")[:8]
     origin = get_object_or_404(POI, pk=692)
     destination = get_object_or_404(POI, pk=695)
-    context = {'poi_list': poi_list, 'origin': origin, 'destination': destination}
+    
+    
+    # Caleb's tests
+    city = request.GET['city']
+    poi_list = POI.objects.filter(city=city)[:8]
+    start_choice = request.GET['startDestination']
+    end_choice = request.GET['endDestination']
+    slider_val = request.GET['points']
+    
+    context = { 'poi_list': poi_list, 
+                'origin': origin, 
+                'destination': destination,
+                'start_choice': start_choice,
+                'end_choice': end_choice,
+                'slider_val': slider_val,
+                'city': city}
     return render(request, 'tour/map.html', context)
