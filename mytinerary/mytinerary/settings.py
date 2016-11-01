31d1f23f7efc182fa87b6ae9baac1d18c7b9d5bc
Caleb Braun <calebjbraun@gmail.com>
2016-11-01 16:54:25 -0500

    Cleaning up file structure

--- mytinerary/mytinerary/settings.py
+++ mytinerary/mytinerary/settings.py
@@ -122,4 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = '/Library/WebServer/wsgi-webroot/TourItineraryComps/mytinerary/static/'
 STATIC_URL = '/static/'
--- mytinerary/tour/views.py
+++ mytinerary/tour/views.py
@@ -9,15 +9,15 @@ def index(request):
 #GET: tour/map/
 def map(request):
 #    poi = get_object_or_404(POI, pk=1)
-#    poi_list = get_list_or_404(POI, city="Minneapolis")
-#    poi_list = POI.objects.filter(city="Minneapolis")[:8]
+    poi_list = get_list_or_404(POI, city="Minneapolis")
+    poi_list = POI.objects.filter(city="Minneapolis")[:8]
     origin = get_object_or_404(POI, pk=692)
     destination = get_object_or_404(POI, pk=695)
     
     
     # Caleb's tests
     city = request.GET['city']
-    poi_list = POI.objects.filter(city=city)[:8]
+#    poi_list = POI.objects.filter(city=city)[:8]
     start_choice = request.GET['startDestination']
     end_choice = request.GET['endDestination']
     slider_val = request.GET['points']
