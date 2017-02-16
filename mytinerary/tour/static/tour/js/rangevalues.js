97f1cd65026cdb90a365e3c4d4bdefd13cfe570c
tuchowj <tuchowj@carleton.edu>
2017-02-15 21:57:48 -0600

    new sliders

--- mytinerary/tour/static/tour/js/rangevalues.js
+++ mytinerary/tour/static/tour/js/rangevalues.js
@@ -1,19 +1,29 @@
 //$(document).ready(function() {
-//    var quantifiers = ["None", "Few", "Some", "Many", "Lots!"];
-//                    
-//    $(".slider")
-//        .slider({ 
-//            min: 0, 
-//            max: quantifiers.length-1, 
-//            value: 3 
-//        })
-//        .slider("pips", {
-//            rest: "label",
-//            labels: quantifiers
-//        })
+//    
 //});
 
-
+function createSlider(name) {
+    var quantifiers = ["None", "Few", "Some", "Many", "Lots!"];
+    $(name)
+        .slider({ 
+            min: 0, 
+            max: quantifiers.length-1, 
+            value: 2,
+            animate: 500,
+            slide: function( event, ui ) {
+                $(name + "Input").val(ui.value);
+            }
+        })
+        .slider("pips", {
+            first: "pip",
+            last: "pip",
+        })
+        .slider("float", {
+            labels: quantifiers
+        });
+    
+    $(name + "Input").val("2");
+}
 
 function startToEnd() {
   $("#start-to-end-fields").html(`
@@ -36,9 +46,6 @@ function startToEnd() {
         <input type="range" class="form-control slide-input" id="points" name="points" min="0" max="8" step="1"
       oninput="setVal(this.value,'pointValBox')" onchange="setVal(this.value,'pointValBox')">
       </div>
-      <div class="col-sm-2 range-val">
-        <span id="pointValBox"></span>&nbsp; Stops
-      </div>
     </div>
     <div class="form-group row ">
       <label class="col-sm-3 control-label">Directness</label>
@@ -46,41 +53,26 @@ function startToEnd() {
         <input type="range" class="form-control slide-input" id="miles" name="miles" min="0" max="12" step="3"
       oninput="setDirectness(this.value,'distanceValBox')" onchange="setDirectness(this.value,'distanceValBox')">
       </div>
-      <div class="col-sm-2 range-val">
-        <span id="distanceValBox"></span>&nbsp;
-      </div>
     </div>
     <div class="form-group row">
       <label class="col-sm-3 control-label">Museums</label>
       <div class="col-sm-6">
-        <input type="range" id="museums" name="museums" min="0" max="4" step="1"
-    oninput="setQuantifier(this.value,'museumValBox')"
-    onchange="setQuantifier(this.value,'museumValBox')" class="form-control slide-input">
-      </div>
-      <div class="col-sm-2 range-val">
-        <span id="museumValBox"></span>&nbsp;
+        <div id="museums" class="slider"></div>
+        <input type="hidden" id="museumsInput" name="museums">
       </div>
     </div>
     <div class="form-group row">
       <label class="col-sm-3 control-label">Landmarks</label>
       <div class="col-sm-6">
-        <input type="range" id="landmarks" name="landmarks" min="0" max="4" step="1"
-    oninput="setQuantifier(this.value,'landmarkValBox')"
-    onchange="setQuantifier(this.value,'landmarkValBox')" class="form-control slide-input">
-      </div>
-      <div class="col-sm-2 range-val">
-        <span id="landmarkValBox"></span>&nbsp;
+        <div id="landmarks" class="slider"></div>
+        <input type="hidden" id="landmarksInput" name="landmarks">
       </div>
     </div>
     <div class="form-group row">
       <label class="col-sm-3 control-label">Activities</label>
       <div class="col-sm-6">
-        <input type="range" id="activities" name="activities" min="0" max="4" step="1"
-    oninput="setQuantifier(this.value,'activityValBox')"
-    onchange="setQuantifier(this.value,'activityValBox')" class="form-control slide-input">
-      </div>
-      <div class="col-sm-2 range-val">
-        <span id="activityValBox"></span>&nbsp;
+        <div id="activities" class="slider"></div>
+        <input type="hidden" id="activitiesInput" name="activities">
       </div>
     </div>
     <div class="form-group row">
@@ -89,82 +81,21 @@ function startToEnd() {
         <div id="parks" class="slider"></div>
         <input type="hidden" id="parksInput" name="parks">
       </div>
-      <div class="col-sm-2 range-val">
-        <div id="parkValBox"></div>&nbsp;
-      </div>
     </div>
     `);
 //    setVal($("#points").val(), "pointValBox");
 //    setDirectness($("#miles").val(), "distanceValBox");
-//    setQuantifier($("#museums").val(), "museumValBox");
-//    setQuantifier($("#landmarks").val(), "landmarkValBox");
-//    setQuantifier($("#activities").val(), "activityValBox");
-//    setQuantifier($("#parks").val(), "parkValBox");
     
-    var quantifiers = ["None", "Few", "Some", "Many", "Lots!"];
-    $(".slider")
-        .slider({ 
-            min: 0, 
-            max: quantifiers.length-1, 
-            value: 2,
-            animate: 300,
-            slide: function( event, ui ) {
-//                $("#parks").val( ui.value );
-                $("#parksInput").val(ui.value);
-//                alert(ui.value);
-            }
-        })
-        .slider("pips", {
-            rest: "label",
-            labels: quantifiers
-        });
+    
+//    createSlider("#miles");
+    createSlider("#museums");
+    createSlider("#landmarks");
+    createSlider("#activities");
+    createSlider("#parks");
+    
     initAutocomplete();
 }
 
 function exploratory() {
   $("#start-to-end-fields").empty();
 }
-
-//function setVal(newVal, idName){
-//    $("#" + idName).html(newVal);
-//}
-//
-//function setQuantifier(newVal, idName) {
-//    switch(newVal) {
-//        case "0":
-//            $("#" + idName).html("None");
-//            break;
-//        case "1":
-//            $("#" + idName).html("Few");
-//            break;
-//        case "2":
-//            $("#" + idName).html("Some");
-//            break;
-//        case "3":
-//            $("#" + idName).html("Many");
-//            break;
-//        case "4":
-//            $("#" + idName).html("Lots!");
-//            break;
-//    }
-//}
-//
-//function setDirectness(newVal, idName) {
-//    switch(newVal) {
-//        case "0":
-//            $("#" + idName).html("In a Rush");
-//            break;
-//        case "3":
-//            $("#" + idName).html("More Direct");
-//            break;
-//        case "6":
-//            $("#" + idName).html("Default");
-//            break;
-//        case "9":
-//            $("#" + idName).html("Scenic Route");
-//            break;
-//        case "12":
-//            $("#" + idName).html("Only the Best!");
-//            break;
-//    }
-//}
