664bbf59fc8f0db187dd3fd556c0775e16f6d2e3
Caleb Braun <calebjbraun@gmail.com>
2017-02-21 23:05:35 -0600

    Moved some code to phonemodal.js, updated some comments

--- mytinerary/tour/static/tour/js/phonemodal.js
+++ mytinerary/tour/static/tour/js/phonemodal.js
@@ -1,9 +1,69 @@
-//Plugin options and our code
+/**
+ * Sets the options for the modal
+ */
 $("#modal_trigger").leanModal({
-        top: 100,
+    top: 100,
     overlay: 0.6,
     closeButton: ".modal_close"
 });
+
+
+/**
+ * Given a url, gets a phone number from the phone input and sends the url to
+ * that number.
+ */
+function sendText(url) {
+    var phoneNumber = $('input[name=phone1]').val() + $('input[name=phone2]').val() + $('input[name=phone3]').val()
+
+    $.ajax({
+        url : "/tour/send_directions/",
+        type : 'GET',
+        data : {'url' : url,
+                'number' : phoneNumber},
+        success : function(success) {
+            closeSendDirectionsWindow(success);
+        },
+        cache : false,
+        error : function(xhr, errmsg, err) {
+            window.open(url, "_blank");
+            console.log(errmsg);
+            console.log(xhr.status + " " + xhr.responseText);
+        }
+    });
+}
+
+function getDirectionsURL(directionsDisplay) {
+    var url = 'https://www.google.com/maps/dir';
+    var directionsAddresses = []
+    var route = directionsDisplay.getDirections().routes[0];
+    if (route.legs.length > 13) {
+        alert("Google maps cannot render this many points");
+        return;
+    }
+    for (var i = 0; i < route.legs.length; i++) {
+        if (i == 0) {
+            directionsAddresses.push(route.legs[i].start_address);
+        }
+        directionsAddresses.push(route.legs[i].end_address);
+    }
+
+    for (var i = 0; i < directionsAddresses.length; i++) {
+        url += "/" + directionsAddresses[i];
+    }
+    url = url.replace(/\s/g, "+");
+    url = url.replace(/,/g, "");
+    url += "/data=!4m2!4m1!3e2"; // Data for making travel mode walking
+
+    return url;
+}
+
+
+function closeSendDirectionsWindow(success) {
+    console.log(success);
+    $('.one_half').prop('onclick',null).off('click');
+    console.log($('.one_half'));
+    $('.one_half').text('sent!');
+}
 
 $(function() {
  //       Calling Login Form
@@ -30,4 +90,3 @@ $(function() {
                                 return false;
                                     });
 });
-
